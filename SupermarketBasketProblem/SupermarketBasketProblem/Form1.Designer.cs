8417c16c59e2e6ac4c67c1909f0343ace6b7aa45
Yeliz Erdoğan <44113028+yelizerdogan@users.noreply.github.com>
2021-07-01 21:53:17 +0300

    Add files via upload

---
+++ SupermarketBasketProblem/SupermarketBasketProblem/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SupermarketBasketProblem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lsbFieldNames = new System.Windows.Forms.ListBox();
+            this.lsbRules = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 12);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(525, 36);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "HESAPLA";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lsbFieldNames
+            // 
+            this.lsbFieldNames.FormattingEnabled = true;
+            this.lsbFieldNames.Location = new System.Drawing.Point(12, 54);
+            this.lsbFieldNames.Name = "lsbFieldNames";
+            this.lsbFieldNames.Size = new System.Drawing.Size(334, 355);
+            this.lsbFieldNames.TabIndex = 1;
+            // 
+            // lsbRules
+            // 
+            this.lsbRules.FormattingEnabled = true;
+            this.lsbRules.Location = new System.Drawing.Point(352, 54);
+            this.lsbRules.Name = "lsbRules";
+            this.lsbRules.Size = new System.Drawing.Size(730, 355);
+            this.lsbRules.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 454);
+            this.Controls.Add(this.lsbRules);
+            this.Controls.Add(this.lsbFieldNames);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lsbFieldNames;
+        private System.Windows.Forms.ListBox lsbRules;
+    }
+}
+
---
+++ SupermarketBasketProblem/SupermarketBasketProblem/Form1.cs
@@ -0,0 +1,81 @@
+﻿using Cotur.DataMining.Association;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupermarketBasketProblem
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataFields dataFields = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Alışveriş fişlerinde bulunan ürünlerin listesi
+
+            List<string> FieldNames = new List<string>() { "Su", "Ekmek", "Yumurta", "Peynir", "Cips", "Kola", "Oyuncak" };
+
+
+
+            // Alışveriş fişleri - hangi ürünler alınmış?
+
+            List<List<int>> Transactions = new List<List<int>>() {
+                new List<int>(){0,1,2,3,4,5,6},
+                new List<int>(){0,2,3,5,6},
+                new List<int>(){0,1,2,3},
+                new List<int>(){2,4,6},
+                new List<int>(){1,2,4,6},
+                new List<int>(){4,5},
+                new List<int>(){2,3,4},
+                new List<int>(){0,2,4,6},
+                new List<int>(){1,3,5}
+            };
+
+            // ürün çeşidi miktarı, alışveriş listesi, ürün isimleri listesiyle oluşturulan dataFields
+            dataFields = new DataFields(7, Transactions, FieldNames);
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            // dataFields nesnesine göre oluşturulan Apriori
+            Apriori apriori = new Apriori(dataFields);
+
+            // Minumum destek oranı 0.25 ile hesaplanması // eşik değer
+            apriori.CalculateCNodes(0.25f);
+            
+
+
+            foreach (var levels in apriori.EachLevelOfNodes)
+            {
+                
+                foreach (var node in levels)
+                {
+                    // Birliktelik tabloları
+                    lsbFieldNames.Items.Add(node.ToDetailedString(dataFields).ToString());
+                   
+                }
+                lsbFieldNames.Items.Add("--------------------------");
+            }
+            foreach (var rules in apriori.Rules)
+            {
+                // Birliktelik kuralları
+                lsbRules.Items.Add(rules.ToDetailedString(dataFields).ToString());
+            }
+
+        }
+
+    }
+}
